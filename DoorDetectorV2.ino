3b63b3257ddc41e8a8aa3e2a7196d7ca484ca655
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2018-11-23 02:17:18 +0800

    Add files via upload
    Optimization. 
    
    Need to change the tune to something nicer.

---
+++ DoorDetectorV2.ino
@@ -0,0 +1,39 @@
+int buzzer = 13;             // Passive buzzer module
+int sensor = 2;              // Passive IR sensor
+int state = LOW;             // Default state = OFF; state = ON/OFF; ON = person detected, OFF = no person
+int val = 0;                 // IR sensor variable 
+unsigned char i;             // For counting
+
+void setup() {
+  pinMode(buzzer, OUTPUT);     
+  pinMode(sensor, INPUT);    
+  Serial.begin(9600);        
+}
+
+void loop(){
+  val = digitalRead(sensor);   // Read from IR sensor 
+  if (val == HIGH) {           // If value is HIGH, sensor detected something
+      for (i = 0; i <2; i++)      // Sound buzzer
+      {
+         digitalWrite (buzzer, HIGH) ; // Future update could make the ringtone nicer?
+         delay (1) ;
+         digitalWrite (buzzer, LOW) ; 
+         delay (1) ;
+      }       
+    delay(100);               
+    
+    if (state == LOW) {
+      Serial.println("Person detected!");  // Serial print for troubleshooting
+      state = HIGH ;            // Change state to "person detected"
+    }
+  } 
+  else {
+      digitalWrite(buzzer, LOW); // Ensure buzzer off 
+      delay(200);            //
+      
+      if (state == HIGH){ // State is HIGH from previous loop.
+        Serial.println("Nothing is happening.");  // Serial print for troubleshooting
+        state = LOW;       // Reset state to LOW
+    }
+  }
+}
